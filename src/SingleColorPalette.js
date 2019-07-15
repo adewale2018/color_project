d5f3c59cfcd9e04539e9e2b1e4dd1a343fcfb54b
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-07-15 11:00:28 +0100

    Add sigleColorPalette component

---
+++ src/SingleColorPalette.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Palette from './Palette';
+
+class SingleColorPalette extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Single Color Palette</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleColorPalette;
